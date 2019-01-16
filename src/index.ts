70ea855afe7953b55eaf0e6536b8a88b94089e88
Hiroyuki Tanaka <h.tanaka.0325@gmail.com>
2019-01-16 21:20:02 +0900

    雑にCORS

--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,11 @@ createConnection()
   .then(async connection => {
     const app = express()
     app.use(bodyParser.json())
+    app.use(function(req, res, next) {
+      res.header("Access-Control-Allow-Origin", "*")
+      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+      next()
+    })
 
     // register express routes from defined application routes
     Routes.forEach(route => {
